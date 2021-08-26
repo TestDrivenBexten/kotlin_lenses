574bc2022baafe51cf0c2cc5fae369ae7676b229
Skyler Bexten <skyler.bexten@gmail.com>
2021-08-25 20:17:25 -0600

    Handle if-else

--- src/main/kotlin/FleetOperations.kt
+++ src/main/kotlin/FleetOperations.kt
@@ -1,8 +1,12 @@
 
 fun decommissionShip(fleet: Fleet, squadName: String, shipName: String): Fleet {
     val newSquadList = fleet.registry.map { squadron ->
-        val newShipList = squadron.registry.filter { !shipName.equals(it.shipName) }
-        Squadron(squadron.squadName, newShipList)
+        if(squadron.squadName.equals(squadName)){
+            val newShipList = squadron.registry.filter { !shipName.equals(it.shipName) }
+            Squadron(squadron.squadName, newShipList)
+        } else {
+            squadron
+        }
     }
     return Fleet(fleet.fleetName, newSquadList)
 }
