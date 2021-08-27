5d1910e188d3b4e395dcb191f4ac1f571ba64af3
Skyler Bexten <skyler.bexten@gmail.com>
2021-08-26 21:05:44 -0600

    Traversal is a work in progress

--- src/main/kotlin/ArrowOperations.kt
+++ src/main/kotlin/ArrowOperations.kt
@@ -1,13 +1,29 @@
 import arrow.optics.Lens
+import arrow.optics.Traversal
 
 val starshipName: Lens<Starship, String> = Lens(
     get = { it.shipName },
     set = { starship, shipName -> starship.copy(shipName = shipName)}
 )
 
+val starhips: Lens<Squadron, List<Starship>> = Lens(
+    get = { it.registry },
+    set = { squadron, registry -> squadron.copy(registry = registry)}
+)
+
+val everyStarship = Traversal.list<Starship>()
+
 fun arrowRenameStarship(starship: Starship, shipName: String): Starship {
     val shipLambda = { _: String -> shipName}
     return starshipName.modify(starship, shipLambda)
+}
+
+fun renameShipBySerialNumber(starship: Starship, serialNumber: String, shipName: String): Starship{
+    return if (starship.serialNumber.equals(serialNumber)){
+        starship.copy(shipName = shipName)
+    } else {
+        starship
+    }
 }
 
 fun arrowRenameShipInSquadron(squadron: Squadron, serialNumber: String, shipName: String) : Squadron {
